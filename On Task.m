7720dba1e726bdf91a6ae4191fe39285eeb1dee3
Teddy Wing <ted@teddywing.com>
2012-07-20 23:13:14 -0400

    Added category from Matt Gallagher to get the Application Support director for my application. Didn't end up working, 10.5+ only, even after my edits.

--- On Task.m
+++ On Task.m
@@ -1,10 +1,15 @@
 #import <Foundation/Foundation.h>
 
+#import "NSFileManager+DirectoryLocations.h"
+
 int main (int argc, const char * argv[]) {
     NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
-
-    // insert code here...
-    NSLog(@"Hello, World!");
+	
+	NSString *supportPath = [[NSFileManager defaultManager] applicationSupportDirectory];
+	
+    // Take screenshot
+	NSLog(@"%@", supportPath);
+	
     [pool release];
     return 0;
 }
