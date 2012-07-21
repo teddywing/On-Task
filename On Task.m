87306f8ca78901b1d6e79dc74428d32e29193f63
Teddy Wing <ted@teddywing.com>
2012-07-21 00:48:48 -0400

    Now takes a screenshot and calls it the current date & time.

--- On Task.m
+++ On Task.m
@@ -5,10 +5,23 @@
 int main (int argc, const char * argv[]) {
     NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
 	
+	// Get Application Support dir path
 	NSString *supportPath = [[NSFileManager defaultManager] applicationSupportDirectory];
 	
+	// Get a date string
+	[NSDateFormatter setDefaultFormatterBehavior:NSDateFormatterBehavior10_4];
+	NSDateFormatter *df = [[NSDateFormatter alloc] init];
+	[df setDateFormat:@"yyyyMMddHHmmss"];
+	NSString *date = [df stringFromDate:[NSDate date]];
+	
     // Take screenshot
-	NSLog(@"%@", supportPath);
+	NSTask *takeScreenshot = [[NSTask alloc] init];
+	[takeScreenshot setLaunchPath:@"/usr/sbin/screencapture"];
+	[takeScreenshot setArguments:[NSArray arrayWithObjects:@"-SxC", [supportPath stringByAppendingString:[NSString stringWithFormat:@"/%@.png", date]], nil]];
+	[takeScreenshot launch];
+	[takeScreenshot release];
+	
+	[df release];
 	
     [pool release];
     return 0;
