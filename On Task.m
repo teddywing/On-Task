02a72745332764ca4e7ed48b0d26fe5e083ecefc
Teddy Wing <ted@teddywing.com>
2012-07-21 01:34:14 -0400

    Got system beep to play (the one I wanted). Finally.

--- On Task.m
+++ On Task.m
@@ -1,4 +1,4 @@
-#import <Foundation/Foundation.h>
+#import <AppKit/AppKit.h>
 
 #import "NSFileManager+DirectoryLocations.h"
 
@@ -14,12 +14,24 @@ int main (int argc, const char * argv[]) {
 	[df setDateFormat:@"yyyyMMddHHmmss"];
 	NSString *date = [df stringFromDate:[NSDate date]];
 	
-    // Take screenshot
+    // # Take screenshot
 	NSTask *takeScreenshot = [[NSTask alloc] init];
 	[takeScreenshot setLaunchPath:@"/usr/sbin/screencapture"];
 	[takeScreenshot setArguments:[NSArray arrayWithObjects:@"-SxC", [supportPath stringByAppendingString:[NSString stringWithFormat:@"/%@.png", date]], nil]];
 	[takeScreenshot launch];
+	[takeScreenshot waitUntilExit];
 	[takeScreenshot release];
+	
+	
+	// # Play system beep
+	NSSound *beep = [NSSound soundNamed:@"Tink"];
+	[beep play];
+	while ([beep isPlaying]) {
+		// Don't exit the program before the sound finishes
+	}
+	
+	
+	// # Open text log
 	
 	[df release];
 	
